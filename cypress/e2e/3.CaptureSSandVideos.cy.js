811292ba8c2fd353c3a0b8510fdd380c8388ce35
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-12-12 13:01:08 +0530

    Automatically capture SS when failures using only terminal run

--- cypress/e2e/3.CaptureSSandVideos.cy.js
+++ cypress/e2e/3.CaptureSSandVideos.cy.js
@@ -1,9 +1,20 @@
 describe('Test 3',()=>
 {
-    it("Capture SS and Videos", () => {
+    /*it("Capture SS", () => {
         cy.visit("www.google.com");
         cy.screenshot("Home page");
-       // cy.wait(5000);
+        cy.wait(5000);
         cy.get("img[alt='Google']").screenshot("Logo");
+
+    }) */
+
+    it("Capture failures SS and Videos", () => {
+        cy.visit("https://demo.opencart.com");
+
+        //Automatically capture SS & video on failure - only when on execute through CLI
+        cy.get("li:nth-child(7) a:nth-child(1)").click(); //Camera Page
+
+        cy.get("div[id='content'] h2").should('have.text',"Tablets");
     })
+    
 })
